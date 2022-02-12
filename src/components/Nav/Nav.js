6d3781b88f0dcd621ae002c0c89445af6d1de62b
Saloni Yadav <saloniy97@gmail.com>
2022-02-12 11:22:30 -0500

    Re-styled header; added font-awesome for any icons needed

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,21 +1,30 @@
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavItem from 'react-bootstrap/NavItem';
 import Container from 'react-bootstrap/Container';
 
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPowerOff } from '@fortawesome/free-solid-svg-icons';
+
+import './Nav.css';
 
 function Navigation() {
     return (
         <>
-            <Navbar bg="dark" variant="dark" expand="lg">
+            <Navbar className="drop-shadow" bg="dark" variant="dark" expand="lg">
                 <Container>
-                    <Navbar.Brand as={Link} to="/">Sports</Navbar.Brand>
-                    <Nav className="me-auto">
-                        <Navbar.Brand as={Link} to="/" className="nav-link">Products</Navbar.Brand>
-                        <Navbar.Brand as={Link} to="/my-orders" className="nav-link" >My Orders</Navbar.Brand>
-                        <Navbar.Brand as={Link} to="/cart" className="nav-link" >Your Cart</Navbar.Brand>
-                    </Nav>
+                    <Navbar.Brand as={Link} to="/">Logo</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav" className='justify-content-end d-flex'>
+                        <Nav>
+                            <Nav.Link as={Link} to="/" className="nav-link pe-3">Products</Nav.Link>
+                            <Nav.Link as={Link} to="/my-orders" className="nav-link pe-3" >My Orders</Nav.Link>
+                            <Nav.Link as={Link} to="/cart" className="nav-link pe-3" >My Cart</Nav.Link>
+                            <Nav.Link>
+                                <FontAwesomeIcon icon={faPowerOff} /> Logout
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
                 </Container>
             </Navbar>
         </>
